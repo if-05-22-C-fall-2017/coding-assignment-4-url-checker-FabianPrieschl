3fe755cae8d1d93506f2e02bf8cf1572c66587cd
FabianPrieschl <fabianprieschl@gmail.com>
2018-04-26 09:41:44 +0200

    URL-Coding-Assignment
    Reuploaded as a maven project instead of an ant project as discussed in the lesson.

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -1,6 +1,6 @@
 /*
- * Copyright ©2016. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
- * Department of Informatics and Media Technique, HTBLA Leonding, 
+ * Copyright ©2018. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
+ * Department of Informatics and Media Technique, HTBLA Leonding,
  * Limesstr. 12 - 14, 4060 Leonding, AUSTRIA. All Rights Reserved. Permission
  * to use, copy, modify, and distribute this software and its documentation
  * for educational, research, and not-for-profit purposes, without fee and
@@ -9,31 +9,26 @@
  * appear in all copies, modifications, and distributions. Contact the Head of
  * Informatics and Media Technique, HTBLA Leonding, Limesstr. 12 - 14,
  * 4060 Leonding, Austria, for commercial licensing opportunities.
- * 
+ *
  * IN NO EVENT SHALL HTBLA LEONDING BE LIABLE TO ANY PARTY FOR DIRECT,
  * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
  * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
  * EVEN IF HTBLA LEONDING HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * HTBLA LEONDING SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  * PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
  * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
  * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
-package httpurl;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *
- * @author P. Bauer (p.bauer@htl-leonding.ac.at)
- */
 public class HttpUrlChecker {
-    private static final Pattern p = Pattern.compile("https?:\\/\\/(www\\.)?[a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-z-]{2,6}([a-zA-Z0-9@:%_\\+.~#?&//=]*)");
-    
+
+ private static final Pattern p = Pattern.compile("https?:\\/\\/(www\\.)?[a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-z-]{2,6}([a-zA-Z0-9@:%_\\+.~#?&//=]*)");    
     static Matcher getMatcher(String httpString) {
         return p.matcher(httpString);
     }
+
 }
